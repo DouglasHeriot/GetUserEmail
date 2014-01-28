ad6f0fa03f0a242db0ce569eb79da3ad55b8b30d
Douglas Heriot <douglas@douglasheriot.com>
2014-01-28 20:12:26 +1100

    Initial Commit

---
+++ GetUserEmail/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GetUserEmail
+//
+//  Created by Douglas Heriot on 28/01/2014.
+//  Copyright (c) 2014 Douglas Heriot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
